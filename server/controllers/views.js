460d03238a58d9438356a389df18fcda23942228
sham1lk <khastiev@gmail.com>
2020-05-08 16:31:44 +0300

    some front

--- server/controllers/views.js
+++ server/controllers/views.js
@@ -28,7 +28,7 @@ module.exports = {
     return Attendance
       .findAll({ 
         where: {
-          type : req.body.type
+          type : req.body.type,
           subject: req.body.subject
           
           }})
